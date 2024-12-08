1fe2f7c99cfd9cc420367dd8b2394fb088785dd6
gbroll <gustav.brolin@gmail.com>
2024-12-08 23:14:47 +0100

    Solve day 1 part 1

--- src/day_01.rs
+++ src/day_01.rs
@@ -10,7 +10,17 @@ impl Solution for Day01 {
     }
 
     fn part1(&self, lines: &Vec<String>) -> Result<Self::Item, &str> { 
-        let result = 10;
+        let (left, right): (Vec<u32>, Vec<u32>) = lines
+        .into_iter()
+        .filter(|l| !l.trim().is_empty())
+        .map(|l| {
+            let mut columns = l.split_whitespace().map(|num| num.parse::<u32>().unwrap());
+            (columns.next().unwrap(), columns.next().unwrap())
+        })
+        .unzip();
+
+        let (left_sorted, right_sorted) = (sorted_copy(&left), sorted_copy(&right));
+        let result = sum_pairwise_absolute_diffs(left_sorted, right_sorted);
         return Ok(result);
     }
     
@@ -18,5 +28,19 @@ impl Solution for Day01 {
         let result = 11;
         return Ok(result);
     }
-    
+
+}
+
+fn sorted_copy(input: &Vec<u32>) -> Vec<u32> {
+    let mut output = input.clone();
+    output.sort();
+    output
+}
+
+fn sum_pairwise_absolute_diffs(left: Vec<u32>, right: Vec<u32>) -> u32 {
+    let abs_diff_sum: i32 = left.iter()
+    .zip(right.iter())
+    .map(|(&l, &r)| (l as i32 - r as i32).abs())
+    .sum();
+    return abs_diff_sum as u32
 }
