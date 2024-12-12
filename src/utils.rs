dc83f54fd0d036fdd8a77d10d2490bad087cc34d
gbroll <gustav.brolin@gmail.com>
2024-12-12 21:00:21 +0100

    Improve utils

--- src/utils.rs
+++ src/utils.rs
@@ -2,15 +2,15 @@ use std::fs::read_to_string;
 
 
 pub fn get_input(day: u8)  -> Vec<String> {
-    let mut filename = String::from("input/day_");
-    filename.push_str(format!("{:02}.txt", day).as_str());
-    return read_lines(&filename);
+    let filename = format!("input/day_{:02}.txt", day);
+    read_lines(&filename)
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
-    let mut result = Vec::new();
-    for line in read_to_string(filename).unwrap().lines() {
-        result.push(line.to_string())
-    }
-    result
+    let input_str = read_to_string(filename)
+        .expect(&format!("Error reading file {}", filename));
+    
+    input_str.lines()
+    .map(|line| line.to_string())
+    .collect()
 }
