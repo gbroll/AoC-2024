6f39215d2e71a37c7a538cd99178105be1ce1e7b
gbroll <gustav.brolin@gmail.com>
2024-12-13 23:18:42 +0100

    Solve day 4

--- src/day_04.rs
+++ src/day_04.rs
@@ -19,13 +19,15 @@ impl Solution for Day04 {
     fn part1(&self, lines: &Vec<String>) -> Result<Self::Item, &str> { 
         let grid = to_2d_char_array(lines);
         let search_word = vec!['X', 'M', 'A', 'S'];
-        let word_count = search_grid(&grid, search_word);
+        let word_count = part1_count(&grid, search_word);
         Ok(word_count)
     }
     
     fn part2(&self, lines: &Vec<String>) -> Result<Self::Item, &str> { 
-        let result: u32 = 1;
-        Ok(result)
+        let grid = to_2d_char_array(lines);
+        let search_word = vec!['M', 'A', 'S'];
+        let word_count = part2_count(&grid, search_word);
+        Ok(word_count)
     }
 
 }
@@ -48,8 +50,11 @@ fn get_char_at(x: i32, y: i32, grid: &Vec<Vec<char>>) -> Option<char> {
     None
 }
 
-fn get_char_slice_at(x: u32, y: u32, search_dir: &SearchDir, len: usize, grid: &Vec<Vec<char>>) -> Option<Vec<char>> {
-    
+fn get_char_slice_at(x: i32, y: i32, search_dir: &SearchDir, len: usize, grid: &Vec<Vec<char>>) -> Option<Vec<char>> {
+    if x < 0 || y < 0 {
+        return None;
+    }
+
     let mut slice: Vec<char> = Vec::new();
     let (dx, dy): (i32, i32) = match search_dir {
         SearchDir::Horizontal => (0, 1),
@@ -67,29 +72,45 @@ fn get_char_slice_at(x: u32, y: u32, search_dir: &SearchDir, len: usize, grid: &
         Some(slice)
 }
 
-fn search_grid(grid: &Vec<Vec<char>>, search_word: Vec<char>) -> (u32) {
+fn part1_count(grid: &Vec<Vec<char>>, search_word: Vec<char>) -> u32 {
+    let mut count: u32 = 0;
     let search_dirs = [
         SearchDir::Horizontal,
         SearchDir::Vertical,
         SearchDir::DiagonalLowerLeft,
         SearchDir::DiagonalLowerRight
     ];
-    let mut word_count: u32 = 0;
     for x in 0..grid.len() {
         for y in 0..grid.len() {
             for search_dir in &search_dirs {
-                if let Some(slice) = get_char_slice_at(x as u32, y as u32, &search_dir, search_word.len(), grid) {
+                if let Some(slice) = get_char_slice_at(x as i32, y as i32, &search_dir, search_word.len(), grid) {
                     //println!("{:?} slice at x: {}, y: {}: {:?}", &search_dir, x, y, slice);
-                    if slice == search_word {
-                        word_count += 1
-                    } else if slice.iter().rev().cloned().collect::<Vec<char>>() == search_word {
-                        word_count += 1
+                    if match_words(&slice, &search_word) {
+                        count += 1
                     }
                 }
             }
         }
     }
-    word_count
+    count
 }
    
+fn part2_count(grid: &Vec<Vec<char>>, search_word: Vec<char>) -> u32 {
+    let mut count: u32 = 0;
+    for x in 0..grid.len() {
+        for y in 0..grid.len() {
+            if let Some(slice1) = get_char_slice_at(x as i32, y as i32, &SearchDir::DiagonalLowerLeft, search_word.len(), grid) {
+                if let Some(slice2) = get_char_slice_at(x as i32-2, y as i32, &SearchDir::DiagonalLowerRight, search_word.len(), grid) {
+                    if match_words(&slice1, &search_word) && match_words(&slice2, &search_word) {
+                        count += 1;
+                    }
+                }
+        }
+    }
+}
+    count
+}
 
+fn match_words(first: &Vec<char>, second: &Vec<char>) -> bool {
+    first == second || first == &second.iter().rev().cloned().collect::<Vec<char>>()
+}
