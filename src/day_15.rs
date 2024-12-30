0d43d99c12ebd2e1d928821e725aed80bc6a78ab
gbroll <gustav.brolin@gmail.com>
2024-12-30 23:06:12 +0100

    Solve day 15 part 2

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,5 +1,5 @@
 use std::usize;
-
+use std::collections::{VecDeque, HashSet};
 use crate::Solution;
 
 pub struct Day15;
@@ -20,7 +20,6 @@ fn parse_input(lines: &Vec<String>) -> (Vec<Vec<char>>, (i32, i32), Vec<(i32, i3
                         } else {
                             c
                         }
-    
                     }).collect()
                 )
             } else {
@@ -42,6 +41,24 @@ fn parse_input(lines: &Vec<String>) -> (Vec<Vec<char>>, (i32, i32), Vec<(i32, i3
         }
     }
     (warehouse, start_position, moves)
+}
+
+fn resize_warehouse(warehouse: &Vec<Vec<char>>, start_position: (i32, i32)) -> (Vec<Vec<char>>, (i32, i32)) {
+    let mut resized_warehouse: Vec<Vec<char>> = Vec::new();
+    for row in warehouse {
+        let mut new_row: Vec<char> = Vec::new();
+        for char in row {
+            if *char == 'O' {
+                new_row.push('[');
+                new_row.push(']');
+            } else {
+                new_row.push(*char);
+                new_row.push(*char);
+            }
+        }
+        resized_warehouse.push(new_row);
+    }
+    (resized_warehouse, (start_position.0, start_position.1 * 2))
 }
 
 fn do_move(warehouse: &mut Vec<Vec<char>>, pos: &mut (i32, i32), move_dir: (i32, i32)) -> () {
@@ -70,14 +87,112 @@ fn do_move(warehouse: &mut Vec<Vec<char>>, pos: &mut (i32, i32), move_dir: (i32,
             }
         }
     }
-
 }
 
-fn sum_box_coordinates(warehouse: Vec<Vec<char>>) -> i32 {
+fn get_next_pos(pos: &(i32, i32), move_dir: &(i32, i32)) -> (i32, i32) {
+    (pos.0 + move_dir.0, pos.1 + move_dir.1)
+}
+
+fn get_prev_pos(pos: &(i32, i32), move_dir: &(i32, i32)) -> (i32, i32) {
+    (pos.0 - move_dir.0, pos.1 - move_dir.1)
+}
+
+fn add_box(boxes: &mut VecDeque<(i32, i32)>, pos: &(i32, i32), warehouse: &Vec<Vec<char>>) {
+    boxes.push_back(*pos);
+    if warehouse[pos.0 as usize][pos.1 as usize] == '[' {
+        boxes.push_back((pos.0, pos.1 + 1));
+    } else if warehouse[pos.0 as usize][pos.1 as usize] == ']' {
+        boxes.push_back((pos.0, pos.1 - 1));
+    }
+}
+
+fn do_move_part2(warehouse: &mut Vec<Vec<char>>, pos: &mut (i32, i32), move_dir: (i32, i32)) -> () {
+
+    let mut next_pos = get_next_pos(pos, &move_dir);
+    let mut next_type = warehouse[next_pos.0 as usize][next_pos.1 as usize];
+    
+    // Move freely or hit wall?
+    if next_type == '.' {
+        *pos = next_pos;
+        return
+    } else if next_type == '#' {
+        return
+    }
+
+    // Horizontal move, rather easy, just toggle the braces
+    if move_dir.0 == 0 {
+        let this_pos = next_pos;
+        loop {
+            next_pos = (next_pos.0, next_pos.1 + move_dir.1);
+            next_type = warehouse[next_pos.0 as usize][next_pos.1 as usize];
+            if next_type == '.' {
+                break;
+            } else if next_type == '#' {
+                return
+            }
+        }
+        // Go back and toggle
+        while this_pos.1 != next_pos.1 {
+            let prev_pos = get_prev_pos(&next_pos, &move_dir);
+            warehouse[next_pos.0 as usize][next_pos.1 as usize] = warehouse[prev_pos.0 as usize][prev_pos.1 as usize];
+            next_pos = prev_pos;
+        }
+        *pos = this_pos;
+        warehouse[this_pos.0 as usize][this_pos.1 as usize] = '.';
+
+    } else {
+        // Pushing boxes vertically, worse
+        let mut boxes: VecDeque<(i32, i32)> = VecDeque::new();
+        let mut boxes_to_be_moved: VecDeque<(i32, i32)> = VecDeque::new();
+        add_box(&mut boxes, &next_pos, &warehouse);
+        while !boxes.is_empty() {
+            let box_pos = boxes.pop_front().unwrap();
+            boxes_to_be_moved.push_back(box_pos);
+            let next_pos = get_next_pos(&box_pos, &move_dir);
+            let next_type = warehouse[next_pos.0 as usize][next_pos.1 as usize];
+            if next_type == '[' || next_type == ']' {
+                add_box(&mut boxes, &next_pos, warehouse);
+            } else if next_type == '#' {
+                return
+            }
+        }
+
+        // Move should be OK
+        let mut moved_boxes: HashSet<(i32, i32)> = HashSet::new();
+        while !boxes_to_be_moved.is_empty() {
+            let box_pos = boxes_to_be_moved.pop_back().unwrap();
+            if !moved_boxes.contains(&box_pos)
+            {
+                let target_position = get_next_pos(&box_pos, &move_dir);
+                warehouse[target_position.0 as usize][target_position.1 as usize] = warehouse[box_pos.0 as usize][box_pos.1 as usize];
+                warehouse[box_pos.0 as usize][box_pos.1 as usize] = '.';
+                moved_boxes.insert(box_pos);
+            }
+        }
+        *pos = next_pos;
+    }
+}
+
+#[allow(dead_code)] 
+fn display(warehouse: &Vec<Vec<char>>, pos: &(i32, i32)) -> () {
+    for (row_num, row) in warehouse.iter().enumerate() {
+        for (col, val) in row.iter().enumerate() {
+            if row_num as i32 == pos.0 && col as i32 == pos.1 {
+                print!("{}", '@');
+            } else {
+                print!("{}", val);
+            }
+        }
+        println!("");
+    }
+    println!("");
+}
+
+fn sum_box_coordinates(warehouse: Vec<Vec<char>>, box_char: char) -> i32 {
     warehouse
     .iter().enumerate().map(|(row_num, row)| 
         row.iter().enumerate().filter_map(|(col_num, &val)| {
-            if val == 'O' {
+            if val == box_char {
                 Some(row_num as i32 * 100 + col_num as i32)
             } else {
                 None
@@ -98,13 +213,20 @@ impl Solution for Day15 {
         let mut pos = start_position;
         for move_dir in moves {
             do_move(&mut warehouse, &mut pos, move_dir);
-            println!("{:?}", pos);
+            //display(&warehouse, &pos);
         }
-        Ok(sum_box_coordinates(warehouse))
+        Ok(sum_box_coordinates(warehouse, 'O'))
     }
     
     fn part2(&self, lines: &Vec<String>) -> Result<Self::Item, &str> { 
-        Ok(2)
+        let (warehouse, start_position, moves) = parse_input(lines);
+        let (mut warehouse, start_position) = resize_warehouse(&warehouse, start_position);
+        let mut pos = start_position;
+        //display(&warehouse, &pos);
+        for move_dir in moves {
+            do_move_part2(&mut warehouse, &mut pos, move_dir);
+            //display(&warehouse, &pos);
+        }
+        Ok(sum_box_coordinates(warehouse, '['))
     }
-
 }
